f620645a3220a5b7f56787a9a9a55fa035afdd4c
sbx0 <jsonsnownothing@gmail.com>
2019-02-20 14:43:25 +0800

    修复未登录无法查看文章详情的问题

--- src/main/java/cn/sbx0/zhibei/controller/ArticleController.java
+++ src/main/java/cn/sbx0/zhibei/controller/ArticleController.java
@@ -41,6 +41,13 @@ public class ArticleController extends BaseController<Article, Integer> {
         this.mapper = mapper;
     }
 
+    /**
+     * 客户端获取文章信息
+     *
+     * @param id      id
+     * @param request
+     * @return
+     */
     @LogRecord
     @ResponseBody
     @GetMapping("/id/{id}")
@@ -48,21 +55,17 @@ public class ArticleController extends BaseController<Article, Integer> {
         mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
         json = mapper.createObjectNode();
         User user = userService.getUser(request);
-        if (user != null) {
-            Article article = getService().findById(id);
-            if (article.getAuthor().getId().equals(user.getId())) {
-                mapper.setConfig(mapper.getSerializationConfig().withView(JsonViewInterface.Normal.class));
-                json.put("author", true);
-            } else {
-                mapper.setConfig(mapper.getSerializationConfig().withView(JsonViewInterface.Simple.class));
-                json.put("author", false);
-            }
-            ObjectNode object = mapper.convertValue(article, ObjectNode.class);
-            json.set("object", object);
-            json.put(STATUS_NAME, STATUS_CODE_SUCCESS);
+        Article article = getService().findById(id);
+        if (user != null && article.getAuthor().getId().equals(user.getId())) {
+            mapper.setConfig(mapper.getSerializationConfig().withView(JsonViewInterface.Normal.class));
+            json.put("author", true);
         } else {
-            json.put(STATUS_NAME, STATUS_CODE_NOT_LOGIN);
+            mapper.setConfig(mapper.getSerializationConfig().withView(JsonViewInterface.Simple.class));
+            json.put("author", false);
         }
+        ObjectNode object = mapper.convertValue(article, ObjectNode.class);
+        json.set("object", object);
+        json.put(STATUS_NAME, STATUS_CODE_SUCCESS);
         return json;
     }
 
