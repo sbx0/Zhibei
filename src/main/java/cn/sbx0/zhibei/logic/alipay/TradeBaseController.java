99cbbfd2e0c9e5b6dff3e9967b9b42b4fe342760
sbx0 <jsonsnownothing@gmail.com>
2020-03-24 15:21:35 +0800

    修复付款后无法正常跳转的问题

--- src/main/java/cn/sbx0/zhibei/logic/alipay/TradeBaseController.java
+++ src/main/java/cn/sbx0/zhibei/logic/alipay/TradeBaseController.java
@@ -4,20 +4,14 @@ import cn.sbx0.zhibei.annotation.LoginRequired;
 import cn.sbx0.zhibei.logic.BaseController;
 import cn.sbx0.zhibei.logic.BaseService;
 import cn.sbx0.zhibei.logic.ReturnStatus;
-import cn.sbx0.zhibei.logic.user.base.UserBase;
 import cn.sbx0.zhibei.logic.user.base.UserBaseService;
 import cn.sbx0.zhibei.logic.user.info.UserInfo;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/trade/base")
@@ -39,12 +33,16 @@ public class TradeBaseController extends BaseController<TradeBase, Integer> {
      */
     @LoginRequired
     @GetMapping(value = "/recharge")
-    public ObjectNode recharge(HttpServletRequest request, HttpServletResponse response) {
+    public ObjectNode recharge(Double price) {
         ObjectNode json = initJSON();
+        if (price <= 0.00) {
+            json.put(statusCode, ReturnStatus.invalidValue.getCode());
+            return json;
+        }
         UserInfo user = userBaseService.getLoginUser();
         TradeBase tradeBase = new TradeBase();
         tradeBase.setProductId("recharge");
-        tradeBase.setPrice(100.00);
+        tradeBase.setPrice(price);
         tradeBase.setDiscount(1.00);
         tradeBase.setDiscountType("none");
         tradeBase.setNumbers(1);
